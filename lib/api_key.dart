65f749ad1f815b4b1d398bb05714845739e80a3e
Tuğçe Polat <t.polat2022@gtu.edu.tr>
2024-12-16 23:50:46 +0300

    struct added

---
+++ lib/api_key.dart
@@ -0,0 +1,7 @@
+// lib/api_key.dart
+class ApiKeys {
+  static const String openAI =  'YOUR_API_KEY';
+  static const String firebaseApiKeyAndroid = 'YOUR_API_KEY';
+  static const String firebaseApiKeyIOS = 'YOUR_API_KEY';
+// Diğer API anahtarlarınızı buraya ekleyin
+}
